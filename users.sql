bdda3939758b77e4783c6da4fe875d7158e617e4
SahabatCoding <sahabatcosding29@gmail.com>
2025-02-09 09:25:42 +0700

    insert data users

--- users.sql
+++ users.sql
@@ -1,13 +1,21 @@
+-- Active: 1703338753241@@127.0.0.1@3306@toko
 use toko;
 
-create table users
-(
+create table users (
     username VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
     token VARCHAR(100),
-    PRIMARY KEY(username)
-)engine = InnoDb;
+    PRIMARY KEY (username)
+) engine = InnoDb;
 
 show tables;
 
 describe users;
+
+insert into users (username, password) VALUES ('chairul', 'Rahasia');
+
+select * from users;
+
+UPDATE users
+SET username = 'Chairul'
+WHERE password = "Rahasia";
